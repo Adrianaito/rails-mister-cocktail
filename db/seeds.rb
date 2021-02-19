5eddb1e92d3ed7b4157ebb9c75ca22d539112726
Adriana Ito <ito.dri@gmail.com>
2021-02-19 16:39:45 +0900

    seeding random images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,21 @@
-
+require "open-uri"
 puts 'Cleaning database...'
 Cocktail.destroy_all
 
 puts 'creating cocktails...'
+url = ['https://cutt.ly/Elq5W4B', 'https://cutt.ly/mlq5OZv', 'https://cutt.ly/xlq5Z3T']
 12.times do
   new_cocktail = Cocktail.create!(
     name: Faker::Science.element
   )
+
+
+  file = URI.open(url.sample)
+  new_cocktail.photo.attach(io: file, filename: 'nes.jpeg', content_type: 'image/jpeg')
+  new_cocktail.save
 end
 
 puts "Created #{Cocktail.count} cocktails!"
-
-require 'json'
-require 'open-uri'
 
 puts 'Cleaning database...'
 Ingredient.destroy_all
